fd7102c00484df1fb0a53d050f086e2580eaa753
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-09-20 07:08:14 +0500

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
 
 	return (
-        <nav className="bg-white flex justify-center items-center h-[7vh]">
+        <nav className="bg-white flex justify-center items-center h-[7vh] min-w-96">
             
             <h1 className="text-2xl font-bold text-[rgb(42,123,42)]">AGRIBUSINESS CART</h1>
         </nav>
