6705a72d18df774c847330716dfbc0739c3b9342
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-09-23 02:54:36 +0500

    fixing message position bugs

--- client/src/components/MessageInput.jsx
+++ client/src/components/MessageInput.jsx
@@ -1,7 +1,12 @@
+import { LuSend } from "react-icons/lu"
+
 function MessageInput() {
   return (
-    <div className="fixed bottom-0 flex items-center justify-center m-5 min-w-64">
-    <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md min-w-28 focus:outline-none focus:ring-2 focus:ring-green-600" placeholder="Search..." />
+    <div className="relative flex items-center justify-center m-5 min-w-64">
+    <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md min-w-28 focus:outline-none focus:ring-2 focus:ring-green-600" placeholder="Send a message..." />
+    <button>
+        <LuSend className="absolute right-2 top-1/2 transform -translate-y-1/2"/>
+    </button>
 </div>
   )
 }
