2cc20062844f33839715ed82f35b14891ff479b6
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-10-21 03:42:28 +0500

    FIXING MESSAGES PAGE!

--- server/socket/index.js
+++ server/socket/index.js
@@ -42,54 +42,122 @@ io.on("connection", async (socket) => {
     onlineUsers.add(user._id.toString());
     io.emit("onlineUser", Array.from(onlineUsers));
 
-    socket.on("message-page", async (receiverId) => {
-        console.log("User ID for messaging:", receiverId);
+//     socket.on("message-page", async (receiverId) => {
+//         console.log("User ID for messaging:", receiverId);
         
-        const conversation = await Conversation.findOne({
-            $or: [
-                { sender: user._id, receiver: receiverId },
-                { sender: receiverId, receiver: user._id }
-            ],
-        }).populate("messages");
+//         const conversation = await Conversation.findOne({
+//             $or: [
+//                 { sender: user._id, receiver: receiverId },
+//                 { sender: receiverId, receiver: user._id }
+//             ],
+//         }).populate("messages");
 
-        if (conversation) {
-            const messages = await Message.find({ _id: { $in: conversation.messages } });
-            socket.emit("load-messages", messages);
-        }
-    });
+//         if (conversation) {
+//             const messages = await Message.find({ _id: { $in: conversation.messages } });
+//             socket.emit("load-messages", messages);
+//         }
+//     });
 
-    socket.on("send-message", async ({ receiver, sender, text }) => {
-        try {
-            const message = await Message.create({
-                text,
-                sender,
-                receiver
-            });
+//     socket.on("send-message", async ({ receiver, sender, text }) => {
+//         try {
+//             const message = await Message.create({
+//                 text,
+//                 sender,
+//                 receiver
+//             });
 
-            await Conversation.findOneAndUpdate(
-                { $or: [{ sender, receiver }, { sender: receiver, receiver: sender }] },
-                { $push: { messages: message._id } },
-                { new: true, upsert: true }
-            );
-            
-            socket.to(receiver).emit("receive-message", {
-                _id: message._id,
-                receiver,
-                sender,
-                text,
-                createdAt: message.createdAt,
-            });
+//             await Conversation.findOneAndUpdate(
+//                 { $or: [{ sender, receiver }, { sender: receiver, receiver: sender }] },
+//                 { $push: { messages: message._id } },
+//                 { new: true, upsert: true }
+//             );
 
-        } catch (error) {
-            console.error("Error sending message:", error);
-        }
-    });
+//             socket.to(receiver).emit("receive-message", {
+//                 _id: message._id,
+//                 receiver,
+//                 sender,
+//                 text,
+//                 createdAt: message.createdAt,
+//             });
 
-    socket.on("disconnect", () => {
-        onlineUsers.delete(user._id.toString());
-        io.emit("onlineUser", Array.from(onlineUsers));
-        console.log(`${socket.id} disconnected!`);
-    });
+//         } catch (error) {
+//             console.error("Error sending message:", error);
+//         }
+//     });
+
+//     socket.on("disconnect", () => {
+//         onlineUsers.delete(user._id.toString());
+//         io.emit("onlineUser", Array.from(onlineUsers));
+//         console.log(`${socket.id} disconnected!`);
+//     });
+
+socket.on("message-page", async (receiverId) => {
+    console.log("User ID for messaging:", receiverId);
+    
+    // Find an existing conversation between the current user and the receiver
+    const conversation = await Conversation.findOne({
+        $or: [
+            { sender: user._id, receiver: receiverId },
+            { sender: receiverId, receiver: user._id }
+        ],
+    }).populate("messages");
+
+    if (conversation) {
+        const messages = await Message.find({ _id: { $in: conversation.messages } });
+        socket.emit("load-messages", messages);
+    } else {
+        // No conversation found yet, so no messages to load
+        socket.emit("load-messages", []);
+    }
 });
+
+socket.on("send-message", async ({ receiver, sender, text }) => {
+    try {
+        // Find an existing conversation or create a new one
+        let conversation = await Conversation.findOne({
+            $or: [{ sender, receiver }, { sender: receiver, receiver: sender }],
+        });
+
+        if (!conversation) {
+            // Create a new conversation if one doesn't exist
+            conversation = await Conversation.create({
+                sender,
+                receiver,
+                messages: [],
+            });
+        }
+
+        // Create the message
+        const message = await Message.create({
+            text,
+            sender,
+            receiver
+        });
+
+        // Add the message to the conversation
+        conversation.messages.push(message._id);
+        await conversation.save();
+
+        // Emit the message to the receiver's socket
+        socket.to(receiver).emit("receive-message", {
+            _id: message._id,
+            receiver,
+            sender,
+            text,
+            createdAt: message.createdAt,
+        });
+
+    } catch (error) {
+        console.error("Error sending message:", error);
+    }
+});
+
+socket.on("disconnect", () => {
+    onlineUsers.delete(user._id.toString());
+    io.emit("onlineUser", Array.from(onlineUsers));
+    console.log(`${socket.id} disconnected!`);
+});
+});
+
 
 module.exports = { server, app };
